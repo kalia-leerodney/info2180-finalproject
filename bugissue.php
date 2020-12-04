24f98fa50794c294efffe2916abff0cabf6fc704
Krishna Edwards <krishna.eewards1512@gmail.com>
2020-12-04 12:27:05 -0500

    bug.php

--- bugissue.php
+++ bugissue.php
@@ -7,10 +7,14 @@
     try{
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         echo "You have been connected successfully";
-        $fname= filter_input(INPUT_POST,"firstname",FILTER_SANITIZE_STRING); 
-        $lname= filter_input(INPUT_POST,"lastname",FILTER_SANITIZE_STRING); 
-        $pswrd= filter_input(INPUT_POST,"password",FILTER_SANITIZE_STRING);
-        $mail= filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL); 
+        #$fname= filter_input(INPUT_POST,"firstname",FILTER_SANITIZE_STRING); 
+        #$lname= filter_input(INPUT_POST,"lastname",FILTER_SANITIZE_STRING); 
+        #$pswrd= filter_input(INPUT_POST,"password",FILTER_SANITIZE_STRING);
+        $pswrd="Ffghkjfhgj1";
+        $fname="joe";
+        $lname="bank";
+        $mail="ree@gmail.com";
+        #$mail= filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL); 
         $insert=true;
         $inputs=array($fname,$lname,$pswrd,$mail);
         foreach ($inputs as $i):
@@ -22,7 +26,14 @@
                 echo "empty input";
                 $insert=false; 
             }
-        endforeach;    
+        endforeach;
+        $hashpassword=password_hash($pswrd,PASSWORD_DEFAULT);
+        if ($insert){
+            $stmt->$conn->prepare('INSERT INTO Users(firstname,lastname,_password,email,date_joined)
+            VALUES ($fname,$lname,$hashpassword,$email,NOW());');
+            $stmt->execute();
+            echo"<br>inserted";
+        }   
                 
     
     
