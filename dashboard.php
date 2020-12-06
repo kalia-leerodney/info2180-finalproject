a5ab7282b3d679ea721c1310ce48b0d28aad15b8
HassanahAl-Saba <hassanah.al-saba@hotmail.com>
2020-12-05 21:03:20 -0500

    style 1 of dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -9,47 +9,60 @@
 
 ?>
 
-
-<DOCTYPE html>
-
-<html>
-<head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href = "styles.css">
         <script type="text/javascript" src="dashboard.js"></script>
     </head>
-<body>
-    <div class="container">
-        <header>
-            <img src = "https://lh5.googleusercontent.com/D93o9lawUcDSbkREe5uhbMRnlelySCnuheiFfeUz5F2UPUvDW1Qfdujq4hT0v3waUhe0XX4s-PlJun0UemnuSsZqs9s6wbm-4z-zosxmukyr7rBIdvHtRCvDUuRwuKd49kb1OnBF">
-            <h1> BugMe Issue Tracker </h1>
-        </header>
-        <div class = "sidenav">
-            <ul>
-                <a href="dashboard.php">  Home </a> 
-                <a href="addusers.php">  Add User </a> 
-                <a href="createissue.php">  New Issue </a> 
-                <a href="userlogout.php">  Logout </a>  
-            </ul>  
-        </div>
-
-    <div id="display">
-        <a href="createissue.php" id="createnewissue"> Create New Issue </a>
-        <h1>Issues</h1>
-        <h3> Filter By: </h3>
-        <div class="NavBar">
-            <nav>   
+    <body>
+        <div class="container">
+            <header>
+                <img src = "https://lh5.googleusercontent.com/D93o9lawUcDSbkREe5uhbMRnlelySCnuheiFfeUz5F2UPUvDW1Qfdujq4hT0v3waUhe0XX4s-PlJun0UemnuSsZqs9s6wbm-4z-zosxmukyr7rBIdvHtRCvDUuRwuKd49kb1OnBF">
+                <h1> BugMe Issue Tracker </h1>
+            </header>
+            <div class = "sidenav">
                 <ul>
-                    <li><button id="allissues">  All </button></li>
-                    <li><button id="openissues">  Open </button> </li>
-                    <!-- <li><a href="mytickets.php">  My Tickets </a></li> -->
-                    <li><button id="myissues">  My Tickets </button></li>
-                </ul>
-            </nav>    
+                    <div class = "home">
+                        <img src="MdiHome.svg">
+                        <a href="dashboard.php">Home</a> 
+                    </div>
+
+                    <div class = "adduser">
+                        <img src = "IcBaselineAddCircle.svg">
+                        <a href="addusers.php">Add User</a> 
+                    </div>
+
+                    <div class = "newissue">
+                        <img src = "MdiAccountPlus.svg">
+                        <a href="createissue.php">New Issue</a> 
+                    </div>
+                    <div class = "logout">
+                        <img src = "MdiPower.svg">
+                        <a href="userlogout.php">Logout</a>
+                    </div>
+                </ul>  
+            </div>
+
+            <div class ="display">
+                <div class = "issues">
+                    <h1>Issues</h1>
+                    <a href="createissue.php" id="createnewissue">Create New Issue</a>
+                </div>
+                <div class = "filterby">
+                    <h3> Filter by: </h3>
+                    <div class="navbar">
+                        <nav>   
+                            <ul>
+                                <li><button id="allissues">  All </button></li>
+                                <li><button id="openissues">  Open </button> </li>
+                                <li><button id="myissues">  My Tickets </button></li>
+                            </ul>
+                        </nav>    
+                    </div>
+                </div>
+            <div id="result"></div>
         </div>
-        <div id="result"></div>
-    </div>
-</div>
-</body>
-	
+    </body>
 </html>
